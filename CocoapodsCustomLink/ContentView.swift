d7fa688266f5a639f8842f056aa1e1c334c489dc
kotalab <kotarolab@gmail.com>
2019-07-05 18:21:49 +0900

    setup project

---
+++ CocoapodsCustomLink/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CocoapodsCustomLink
+//
+//  Created by 小島徹也 on 2019/07/05.
+//  Copyright © 2019 com.kotalab. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
