4cd6924aa0b6a5db47cd8ed67b7e8688a0f3942f
kotalab <kotarolab@gmail.com>
2019-07-09 16:16:45 +0900

    always codesign

--- scripts/copy-frameworks.sh
+++ scripts/copy-frameworks.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+set -eu
+
+# This protects against multiple targets copying the same framework dependency at the same time. The solution
+# was originally proposed here: https://lists.samba.org/archive/rsync/2008-February/020158.html
+RSYNC_PROTECT_TMP_FILES=(--filter "P .*.??????")
+
 # Copies and strips a vendored framework
 install_framework()
 {
@@ -36,20 +43,6 @@ install_framework()
   if [[ "$(file "$binary")" == *"dynamically linked shared library"* ]]; then
     strip_invalid_archs "$binary"
   fi
-
-  # Resign the code if required by the build settings to avoid unstable apps
-  code_sign_if_enabled "${destination}/$(basename "$1")"
-
-  # Embed linked Swift runtime libraries. No longer necessary as of Xcode 7.
-  if [ "${XCODE_VERSION_MAJOR}" -lt 7 ]; then
-    local swift_runtime_libs
-    swift_runtime_libs=$(xcrun otool -LX "$binary" | grep --color=never @rpath/libswift | sed -E s/@rpath\\/\(.+dylib\).*/\\1/g | uniq -u)
-    for lib in $swift_runtime_libs; do
-      echo "rsync -auv \"${SWIFT_STDLIB_PATH}/${lib}\" \"${destination}\""
-      rsync -auv "${SWIFT_STDLIB_PATH}/${lib}" "${destination}"
-      code_sign_if_enabled "${destination}/${lib}"
-    done
-  fi
 }
 
 # Strip invalid architectures
@@ -80,28 +73,12 @@ strip_invalid_archs() {
 }
 
 # Signs a framework with the provided identity
-code_sign_if_enabled() {
-  echo 'code_sign_if_enabled'
-  echo "'$EXPANDED_CODE_SIGN_IDENTITY'"
-  if [ -n "${EXPANDED_CODE_SIGN_IDENTITY:-}" -a "${CODE_SIGNING_REQUIRED:-}" != "NO" -a "${CODE_SIGNING_ALLOWED}" != "NO" ]; then
-  echo "code_sign_if_enabledのifのなか"
-    # Use the current code_sign_identity
-    echo "Code Signing $1 with Identity ${EXPANDED_CODE_SIGN_IDENTITY_NAME}"
-    local code_sign_cmd="/usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} ${OTHER_CODE_SIGN_FLAGS:-} --preserve-metadata=identifier,entitlements '$1'"
-
-    if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
-      code_sign_cmd="$code_sign_cmd &"
-    fi
-    echo "$code_sign_cmd"
-    eval "$code_sign_cmd"
-  fi
-}
-
 code_sign() {
   # Use the current code_sign_identitiy
   echo "Code Signing $1 with Identity ${EXPANDED_CODE_SIGN_IDENTITY_NAME}"
-  echo "/usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} --preserve-metadata=identifier,entitlements $1"
-  /usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} --preserve-metadata=identifier,entitlements "$1"
+  local code_sign_cmd="/usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} --preserve-metadata=identifier,entitlements '$1'"
+  echo "$code_sign_cmd"
+  eval "$code_sign_cmd"
 }
 
 if [ "$ACTION" = "install" ]; then
@@ -116,6 +93,16 @@ if [ $SCRIPT_INPUT_FILE_LIST_COUNT -ne 0 ]; then
     inputFileList="${!inputFileListVar}"
     cat "${inputFileList}" | while read inputFile; do
       install_framework "$inputFile"
+
+      for file in $(find ${inputFile} -type f -perm +111); do
+        # Skip non-dynamic libraries
+        if ! [[ "$(file "$file")" == *"dynamically linked shared library"* ]]; then
+          continue
+        fi
+
+        # Resign the code
+        code_sign "${file}"
+      done
     done
   done
 fi
